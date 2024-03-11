f6f8906632fef659273c549ee9638829b0993d2d
Juan Pablo Pinza Armijos <jppinza@espe.edu.ec>
2024-03-10 19:23:04 -0500

    Nav mejor arreglado
    Nav mejor arreglado

--- Tercer Parcial/Proyecto Final/mi-proyecto-web/src/components/Header.jsx
+++ Tercer Parcial/Proyecto Final/mi-proyecto-web/src/components/Header.jsx
@@ -1,49 +1,58 @@
-import React, { useContext } from 'react';
-import '../styles/header.css';
-import { AuthContext } from '../App'; // Importar el contexto de autenticación
+import React, { useContext } from "react";
+import "../styles/header.css";
+import { AuthContext } from "../App"; // Importar el contexto de autenticación
 
 
 function Header() {
-    const { isAuthenticated, handleLogout, nombre } = useContext(AuthContext); // Utilizar useContext para acceder al contexto
+  const { isAuthenticated, handleLogout, nombre } = useContext(AuthContext); // Utilizar useContext para acceder al contexto
 
-    
-
-   
-    
-
-
-
-    return (
-        <header>
-            <a className="linkLogo" href="#">
-                <img className="logo" src="images/ExpenseMaster.png" alt="logo" />
+  return (
+    <header>
+      <a className="linkLogo" href="#">
+        <img className="logo" src="images/ExpenseMaster.png" alt="logo" />
+      </a>
+      <nav>
+        <ul className="linksDelNav">
+          <li>
+            <a
+              target="_blank"
+              href="https://github.com/JuanPabloPinza/ProgramacionWeb"
+            >
+              Sobre Nosotros
             </a>
-            <nav>
-                <ul className="linksDelNav">
-                    <li><a target="_blank" href="https://github.com/JuanPabloPinza/ProgramacionWeb">Sobre Nosotros</a></li>
-                    <li><a href="formularioRegistroFinancieroPersonal.php">Registro Financiero Personal</a></li>
-                    <li><a href="#nuestrosServicios">Nuestros Servicios</a></li>
-                    <li><a href="#preguntasFrecuentes">Q&A</a></li>
-                </ul>
-            </nav>
-
-            
-
-            <a className="CTA" target="_blank" href="https://www.equifax.com.ec/miscreditos/blog/ahorro"><button className="colorBotonPrincipal">Tips de Ahorro</button></a>
-            <hr size="1px" color="#383241" />
-            <hr size="1px" color="rgb(8, 6, 11)" />
-
-            {isAuthenticated && (
+          </li>
+          <li>
+            <a href="formularioRegistroFinancieroPersonal.php">
+              Registro Financiero Personal
+            </a>
+          </li>
+          <li>
+            <a href="#nuestrosServicios">Nuestros Servicios</a>
+          </li>
+          <li>
+            <a href="#preguntasFrecuentes">Q&A</a>
+          </li>
+          {isAuthenticated && (
             <>
-            <span className="nombreUsuario">¡Hola, {nombre || 'Usuario'}!</span>
-            <button onClick={handleLogout} className="colorBotonPrincipal">
-              Cerrar Sesión
-            </button>
+              <li>
+                <span className="nombreUsuario">
+                  ¡Hola, {nombre || "Usuario"}!
+                </span>
+              </li>
+              <li>
+                <button onClick={handleLogout} className="colorBotonPrincipal">
+                  Cerrar Sesión
+                </button>
+              </li>
             </>
-            
-            )}
-        </header>
-    );
+          )}
+        </ul>
+      </nav>
+
+      <hr size="1px" color="#383241" />
+      <hr size="1px" color="rgb(8, 6, 11)" />
+    </header>
+  );
 }
 
 export default Header;
